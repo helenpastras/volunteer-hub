6307dfeaf0c293b3197bf4892ee775613567cb6a
helenpastras <helenapastras@hotmail.com>
2025-10-21 13:18:48 -0400

    landing dash

--- volunteerhub/my_app/views.py
+++ volunteerhub/my_app/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import modelformset_factory
+from .models import Opportunity
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.http import HttpResponse
 
@@ -6,3 +15,26 @@ from django.http import HttpResponse
 def home(request):
     # Send a simple HTML response
     return HttpResponse('<h1>Hello and Welcme to the amazing Volunteer Hub.</h1>')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('landing')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', {'form':form})
+
+
+# CRUD
